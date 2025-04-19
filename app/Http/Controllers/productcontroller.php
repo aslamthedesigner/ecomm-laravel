55e10e0bd0e24fe205278900bc8c26fdb1d4df37
aslamthedesigner <galaxyguesthouse7@gmail.com>
2025-04-19 13:10:52 +0530

    add cart

--- app/Http/Controllers/productcontroller.php
+++ app/Http/Controllers/productcontroller.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\product;
+use App\Models\cart;
 
 class productcontroller extends Controller
 {
@@ -13,10 +14,30 @@ class productcontroller extends Controller
         return view('product', ['products' => $data]);
         // return $data;
     }
-    function details($id){
-         $data = product::find($id);
+    function details($id)
+    {
+        $data = product::find($id);
 
-        return view('details', ['product'=> $data]);
+        return view('details', ['product' => $data]);
         // return $data;
+    }
+    function search(Request $req)
+    {
+        $data = product::where('name', 'like', '%' . $req->input('query') . '%')->get();
+        return view('search', ['products' => $data]);
+    }
+    function addtocart(Request $req)
+    {
+
+        if ($req->session()->has('user')) {
+            $cart = new cart;
+            $cart->user_id = $req->session()->get('user')['id'];
+
+            $cart->product_id = $req->product_id;
+            $cart->save();  
+            return redirect('/');
+        } else {
+            return redirect('/login');
+        }
     }
 }
